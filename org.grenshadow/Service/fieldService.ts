8674dcc5e6edb96b5cb36298864923b7aa3deeb9
jayawickrama <nishantharaka2004@gmail.com>
2025-01-18 19:34:47 +0530

    created field crud operations

--- org.grenshadow/Service/fieldService.ts
+++ org.grenshadow/Service/fieldService.ts
@@ -5,7 +5,6 @@ const prisma =new PrismaClient();
 export async function saveField(f:fieldDto){
     const newField =await prisma.field.create({
         data:{
-            fieldCode:f.fieldCode,
             name:f.name,
             location:f.location,
             extentSize:f.extentSize,
@@ -14,4 +13,40 @@ export async function saveField(f:fieldDto){
         }
     })
     console.log('field saved successfully');
+}
+
+export async function deleteField(fieldCode:number){
+    try{
+        await prisma.field.delete({
+            where:{fieldCode:fieldCode}
+        })
+        console.log('field deleted successfully!!!')
+
+    }catch (err){
+        console.log('something went wrong...'+err)
+    }
+}
+export async function UpdateField(fieldCode:number,f:fieldDto){
+    try{
+        await prisma.field.update({
+            where :{fieldCode :fieldCode},
+            data:{
+                name:f.name,
+                location:f.location,
+                extentSize:f.extentSize,
+                fieldImage1:f.fieldImage1,
+                fieldImage2:f.fieldImage2
+            }
+
+        })
+    }catch (err){
+        console.log('something went wrong when update this ....')
+    }
+}
+export async  function getAllFields(){
+    try{
+        await prisma.field.findMany();
+    }catch (err){
+        console.log("couldn't load fields  .."+err)
+    }
 }
