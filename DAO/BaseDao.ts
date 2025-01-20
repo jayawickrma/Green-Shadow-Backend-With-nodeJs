350169f789a8b5b978f300087e8ad8bf7389a264
jayawickrama <nishantharaka2004@gmail.com>
2025-01-20 12:59:20 +0530

    creating dao layers to send data to the database

--- DAO/BaseDao.ts
+++ DAO/BaseDao.ts
@@ -1,8 +1,11 @@
 export interface BaseDao<T>{
-
+    // @ts-ignore
     create(name:string,list:T[]);
+    // @ts-ignore
     update(id:number,name:string,list:T[])
+    // @ts-ignore
     delete(id:number)
+    // @ts-ignore
     findAll()
 
 }
