1cfd8baed0cfd399dcb075613982b784f8304f43
jayawickrama <nishantharaka2004@gmail.com>
2025-01-20 20:59:50 +0530

    creating url path dianamically

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import mainRouter from "./org.grenshadow/Routes/MainRouter";
 const app =express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}))
+app.use('api/v1',mainRouter.router)
 
 app.listen(3000,()=>{
   console.log('The server is starting on port 3000')
