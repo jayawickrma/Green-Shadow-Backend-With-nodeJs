58868875cd8f16320d4b9c629a4cb48615945993
jayawickrama <nishantharaka2004@gmail.com>
2025-01-20 19:46:25 +0530

    creating routes path for classes one by one

---
+++ org.grenshadow/Routes/MainRouter.ts
@@ -0,0 +1,13 @@
+import {Router} from "express";
+
+class MainRouter{
+        router : Router;
+        constructor() {
+            this.router.use('/')
+            this.router.use('/')
+            this.router.use('/')
+        }
+}
+
+const mainRouter:MainRouter =new MainRouter();
+export default mainRouter;
--- server.ts
+++ server.ts
@@ -1,9 +1,8 @@
 import express from 'express'
-import CropController from "./org.grenshadow/Controllers/CropController"
 const app =express();
 
 app.use(express.json());
-app.use('/crop',CropController)
+
 
 app.listen(3000,()=>{
   console.log('The server is starting on port 3000')
