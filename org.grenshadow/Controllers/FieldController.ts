d4ef5692f7184080873d3aa5e3a377acd733b239
jayawickrama <nishantharaka2004@gmail.com>
2025-01-20 18:41:28 +0530

    update fields in FieldController.ts done

--- org.grenshadow/Controllers/FieldController.ts
+++ org.grenshadow/Controllers/FieldController.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import {fieldDTO} from "../DTO/FieldDTo";
-import {addField, deleteField, getAllFields} from "../Service/fieldService";
+import {addField, deleteField, getAllFields, updateField} from "../Service/fieldService";
 const router =express.Router();
 
 router.post('/addField',async (req, res)=>{
@@ -29,6 +29,18 @@ router.get('/getAllFields',async (req,res)=>{
     try{
         const fields=await getAllFields();
         res.send(fields)
+    }catch (err){
+        console.log(err)
+        throw new Error("Something went wrong")
+    }
+})
+
+router.put('/updateField:id',async (req,res)=>{
+    const id = req.params.id
+    const fdto:fieldDTO =req.body
+    try{
+        await updateField(id,fdto)
+        res.send('Update Successful...')
     }catch (err){
         console.log(err)
         throw new Error("Something went wrong")
