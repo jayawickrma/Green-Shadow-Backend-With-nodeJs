8111d086ccaa4f016d20cabf59a849f4cb59dd46
jayawickrama <nishantharaka2004@gmail.com>
2025-01-27 22:58:00 +0530

    creating sign In option

--- org.grenshadow/Service/UserService.ts
+++ org.grenshadow/Service/UserService.ts
@@ -2,4 +2,5 @@ import prisma from "../../prisma/Client";
 import {UserDTO} from "../DTO/UserDTO";
 
 export async function createUser(user: UserDTO, email: any, bcryptPw: any){}
+// @ts-ignore
 export async function findByEmail(email){}
