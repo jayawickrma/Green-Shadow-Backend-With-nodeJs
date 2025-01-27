b437a260ccf7f57343ae17bf5a5cb3c4c1119dde
jayawickrama <nishantharaka2004@gmail.com>
2025-01-27 22:55:55 +0530

    creating authentication part

--- org.grenshadow/Controllers/UserController.ts
+++ org.grenshadow/Controllers/UserController.ts
@@ -1,5 +1,5 @@
 import {UserDTO} from "../DTO/UserDTO";
-import {createUser} from "../Service/UserService";
+import {createUser, findByEmail} from "../Service/UserService";
 
 const express = require('express');
 const bcrypt = require('bcrypt');
@@ -19,6 +19,29 @@ class UserController{
             resp.json(err)
         }
     }
-    async signIn(req:any,resp:any){}
+    async signIn(req:any,resp:any){
+        const {email,password} =req.body
+        const userLoginDto = new UserLoginDto(email, password);
+        try{
+            const findUser =await findByEmail(userLoginDto.email)
+            // @ts-ignore
+            if (!findUser){
+                return resp.status(404).json({ error: 'User not found!' });
+            }
+
+            // @ts-ignore
+            const isPasswordValid = await bcrypt.compare(userLoginDto.password, findUser.password);
+            if (!isPasswordValid) {
+                return resp.status(401).json({ error: 'Invalid password!' });
+            }
+            // @ts-ignore
+            const token = jwt.sign({ username: findUser.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+            resp.json({ message: 'Login successful!', token });
+
+        }catch (err){
+
+        }
+    }
 }
 export default UserController;
--- org.grenshadow/Service/UserService.ts
+++ org.grenshadow/Service/UserService.ts
@@ -2,4 +2,4 @@ import prisma from "../../prisma/Client";
 import {UserDTO} from "../DTO/UserDTO";
 
 export async function createUser(user: UserDTO, email: any, bcryptPw: any){}
-export async function findByName(){}
+export async function findByEmail(email){}
