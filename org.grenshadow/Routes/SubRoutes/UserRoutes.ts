c247115ac2ffca6e636662a475949e68ee8870f6
jayawickrama <nishantharaka2004@gmail.com>
2025-01-31 23:11:24 +0530

    refreshToken

--- org.grenshadow/Routes/SubRoutes/UserRoutes.ts
+++ org.grenshadow/Routes/SubRoutes/UserRoutes.ts
@@ -14,6 +14,7 @@ class UserRoutes{
     initialStates():void{
         this.router.post('/signIn',this.userController.signIn)
         this.router.post('/signUp',this.userController.signUp)
+        this.router.post('/refreshToken',this.userController.refreshToken)
     }
 }
 const userRoutes:UserRoutes =new UserRoutes()
