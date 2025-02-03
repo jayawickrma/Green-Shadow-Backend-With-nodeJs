aa14aedaf49fca7844ce00f045db8cd2ce79d08d
jayawickrama <nishantharaka2004@gmail.com>
2025-02-03 11:13:51 +0530

    created vehicle controller class

--- org.grenshadow/Controllers/VehicleController.ts
+++ org.grenshadow/Controllers/VehicleController.ts
@@ -1,30 +1,45 @@
+import {addVehicle, deleteVehicle, getAllVehicles, updateVehicle} from "../Service/vehicleService";
+
 class VehicleController{
     async saveVehicle(req:any,resp:any){
         try{
-
+               const save =await addVehicle(req.body)
+                resp.status(201).json("Vehicle saved")
         }catch (err){
             resp.status(500).send(err)
+            console.log(err)
         }
     }
     async deleteVehicle(req:any,resp:any){
+        const id =req.query['id']
         try{
-
+            await deleteVehicle(id)
+            resp.status(200).json("Vehicle deleted...")
         }catch (err){
             resp.status(500).send(err)
+            console.log(err)
         }
     }
     async updateVehicle(req:any,resp:any){
+        const id =req.query['id']
+        const vehicle =req.body
         try{
-
+            await updateVehicle(id,vehicle)
+            resp.status(200).json("Vehicle deleted...")
         }catch (err){
             resp.status(500).send(err)
+            console.log(err)
         }
     }
     async getAllVehicles(req:any,resp:any){
         try{
-
+            const all =await getAllVehicles();
+            if (all){
+                return all;
+            }
         }catch (err){
             resp.status(500).send(err)
+            console.log(err)
         }
     }
 }
